22eaf72d2191182bf7c9f196d5d159399af13361
Matias Alvarez <chipi-007>
2025-02-07 19:57:55 -0300

    Navegacion dinamica

--- src/components/ItemDetalles/ItemDetalles.jsx
+++ src/components/ItemDetalles/ItemDetalles.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getProduct } from "../Asynchornic/Async";
 import "./ItemDetalles.css"; // Importamos el CSS
 
